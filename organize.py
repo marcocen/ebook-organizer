ce8e9f80768b4893f07a1fb2b30700fd5bc7cb10
Marco Centurion <mcenturion@protonmail.com>
2020-03-04 18:06:51 -0300

    Leave files that got no results for last

--- organize.py
+++ organize.py
@@ -20,31 +20,50 @@ parser.add_argument("-s", "--subdirs",
 args = parser.parse_args()
 
 path = args.in_dir
-
 files = os.listdir(path)
-
+files404 = []
 api_url = "https://www.googleapis.com/books/v1"
-for file in files:
+
+def search_book(file):
     filename = os.path.splitext(file)[0]
-    fileext = os.path.splitext(file)[1]
     r=requests.get("{}/volumes?q={}&format=json".format(api_url, filename))
     volumes = json.loads(r.text)
-
-    new_filename = "not_empty"
-    while new_filename != "" and volumes['totalItems'] == 0:
-        new_filename = input("No book was found for this filemane: {} input an alternative username if you want to: ".format(filename))
-        if new_filename != "":
-            r=requests.get("{}/volumes?q={}&format=json".format(api_url, new_filename))
-            volumes = json.loads(r.text)
-
     if volumes['totalItems'] == 0:
-        continue
-    volumeInfo = volumes['items'][0]['volumeInfo']
+        return []
+    return volumes['items'][0]['volumeInfo']
+
+def new_filename(volumeInfo):
     title = volumeInfo['title']
     authors = str.join(', ', volumeInfo['authors'])
     published = datetime.strptime(volumeInfo['publishedDate'], '%Y-%m-%d').year
-    newtitle = "{} - {} ({})".format(authors, title, published)
+    return "{} - {} ({})".format(authors, title, published)
+
+for file in files:
+    volumeInfo = search_book(file)
+    if not volumeInfo:
+        files404.append(file)
+        continue
+    fileext = os.path.splitext(file)[1]
+    newname = new_filename(volumeInfo)
+    destfile = os.path.join(args.out_dir, "{}{}".format(newname,fileext))
     sourcefile = os.path.join(path, file)
-    destfile = os.path.join(args.out_dir, "{}{}".format(newtitle, fileext))
     print("{} ~> {}".format(sourcefile, destfile))
     copy(sourcefile, destfile)
+
+if files404:
+    print("Couldn't find info for {} files.".format(len(files404)))
+    if input("Do you want to provide alternative names to search (y/[n])? ").lower().strip() == 'y':
+        for file in files404:
+            newname = input("Enter an alternative filename for {}: ".format(file))
+            volumeInfo = search_book(newname)
+            while newname != "" and not volumeInfo:
+                newname = input("Enter an alternative filename for {}: ".format(file))
+                volumeInfo = search_book(newname)
+            if newname == "":
+                continue
+            fileext = os.path.splitext(file)[1]
+            newname = new_filename(volumeInfo)
+            destfile = os.path.join(args.out_dir, "{}{}".format(newname,fileext))
+            sourcefile = os.path.join(path, file)
+            print("{} ~> {}".format(sourcefile, destfile))
+            copy(sourcefile, destfile)
